8481dff92df1b0b2b61a62af5f1d294b3b5f4509
Matthias Freßdorf <mfressdorf.mmt-m2014@fh-salzburg.ac.at>
2016-06-19 22:04:10 +0200

    Add unbind; Add comments

--- index.js
+++ index.js
@@ -3,52 +3,101 @@
 var uuid = require('uuid');
 var amqp = require('amqplib');
 
-module.exports = function () {
-  var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-
+/**
+ * TransactionUtility Factory
+ * @param  {Object} config
+ * @param  {string} config.url - amqp connection url
+ * @param  {string} config.exchange - name of the exchange the messages will be sent to
+ * @return {Object}
+ */
+module.exports = function (_ref) {
   var _ref$url = _ref.url;
   var url = _ref$url === undefined ? 'amqp://localhost' : _ref$url;
+  var _ref$exchange = _ref.exchange;
+  var exchange = _ref$exchange === undefined ? 'transactions' : _ref$exchange;
 
   var open = amqp.connect(url);
-  var exchange = 'transactions';
 
+  /**
+   * Send a message to all listeners for a specific id
+   * @param  {string} id
+   * @param  {string} action - the message which will be delivered to the listeners
+   * @return {Promise}
+   */
   function sendMessage(id, action) {
     return open.then(function (conn) {
       return conn.createChannel();
     }).then(function (ch) {
-      ch.publish(exchange, id, new Buffer(JSON.stringify({ id: id, action: action })));
+      return ch.publish(exchange, id, new Buffer(JSON.stringify({ id: id, action: action })));
     });
   }
 
   return {
+    /**
+     * @return {string}
+     */
     generateId: function generateId() {
       return uuid.v1();
     },
 
+    /**
+     * @param  {string} transactionId
+     * @return {Promise}
+     */
     rollback: function rollback(transactionId) {
       sendMessage(transactionId, 'r');
     },
 
+    /**
+     * @param  {string} transactionId
+     * @return {Promise}
+     */
     commit: function commit(transactionId) {
       sendMessage(transactionId, 'c');
     },
 
-    listener: function listener(service, cb) {
+    /**
+     * Sets up a listener queue, which calls a callback function when a message is received
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    listener: function listener(queueName, fn) {
       return open.then(function (conn) {
         return conn.createChannel();
       }).then(function (ch) {
         ch.assertExchange(exchange, 'direct', { durable: true });
-        return ch.assertQueue(service, { durable: true }).then(function (q) {
-          return ch.consume(service, cb, { noAck: true });
+        return ch.assertQueue(queueName, { durable: true }).then(function (q) {
+          return ch.consume(queueName, fn, { noAck: true });
         });
       });
     },
 
-    listen: function listen(service, transactionId) {
+    /**
+     * Binds a specific transactionId routing path between listener queue and exchange
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    listen: function listen(queueName, transactionId) {
       return open.then(function (conn) {
         return conn.createChannel();
       }).then(function (ch) {
-        return ch.bindQueue(service, exchange, transactionId);
+        return ch.bindQueue(queueName, exchange, transactionId);
+      });
+    },
+
+    /**
+     * Unbinds a specific transactionId routing path between listener queue and exchange
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    unbind: function unbind(queueName, transactionId) {
+      return open.then(function (conn) {
+        return conn.createChannel();
+      }).then(function (ch) {
+        return ch.unbindQueue(queueName, exchange, transactionId);
       });
     }
   };
--- src/index.es6
+++ src/index.es6
@@ -1,43 +1,88 @@
 const uuid = require('uuid');
 const amqp = require('amqplib');
 
-module.exports = ({ url: url = 'amqp://localhost' } = {}) => {
+/**
+ * TransactionUtility Factory
+ * @param  {Object} config
+ * @param  {string} config.url - amqp connection url
+ * @param  {string} config.exchange - name of the exchange the messages will be sent to
+ * @return {Object}
+ */
+module.exports = ({ url: url = 'amqp://localhost', exchange: exchange = 'transactions' }) => {
   const open = amqp.connect(url);
-  const exchange = 'transactions';
 
+  /**
+   * Send a message to all listeners for a specific id
+   * @param  {string} id
+   * @param  {string} action - the message which will be delivered to the listeners
+   * @return {Promise}
+   */
   function sendMessage(id, action) {
     return open
       .then(conn => conn.createChannel())
-      .then(ch => {
-        ch.publish(exchange, id, new Buffer(JSON.stringify({ id, action })));
-      });
+      .then(ch => ch.publish(exchange, id, new Buffer(JSON.stringify({ id, action }))));
   }
 
   return {
+    /**
+     * @return {string}
+     */
     generateId: () => uuid.v1(),
 
+    /**
+     * @param  {string} transactionId
+     * @return {Promise}
+     */
     rollback: (transactionId) => {
       sendMessage(transactionId, 'r');
     },
 
+    /**
+     * @param  {string} transactionId
+     * @return {Promise}
+     */
     commit: (transactionId) => {
       sendMessage(transactionId, 'c');
     },
 
-    listener: (service, cb) => {
+    /**
+     * Sets up a listener queue, which calls a callback function when a message is received
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    listener: (queueName, fn) => {
       return open
         .then(conn => conn.createChannel())
         .then(ch => {
           ch.assertExchange(exchange, 'direct', {durable: true});
-          return ch.assertQueue(service, {durable: true})
-            .then(q => ch.consume(service, cb, { noAck: true }));
+          return ch.assertQueue(queueName, {durable: true})
+            .then(q => ch.consume(queueName, fn, { noAck: true }));
         });
     },
 
-    listen: (service, transactionId) => {
+    /**
+     * Binds a specific transactionId routing path between listener queue and exchange
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    listen: (queueName, transactionId) => {
       return open
         .then(conn => conn.createChannel())
-        .then(ch => ch.bindQueue(service, exchange, transactionId));
+        .then(ch => ch.bindQueue(queueName, exchange, transactionId));
+    },
+
+    /**
+     * Unbinds a specific transactionId routing path between listener queue and exchange
+     * @param  {string} queueName - name of the listener queue
+     * @param  {function} fn - function which takes 1 parameter (msg) and is called when a message is received
+     * @return {Promise}
+     */
+    unbind: (queueName, transactionId) => {
+      return open
+        .then(conn => conn.createChannel())
+        .then(ch => ch.unbindQueue(queueName, exchange, transactionId));
     }
   }
 };
